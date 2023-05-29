d2400901d3207eb224d9cfd25d8957901c017653
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-30 00:12:34 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,18 +3,22 @@ import torch
 from transformers import BartTokenizer, BartForConditionalGeneration
 
 def summarize_text(text):
-    # Load the pre-trained BART model
-    model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-    tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+    try:
+        # Load the pre-trained BART model
+        model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+        tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
 
-    # Tokenize the input text
-    input_ids = tokenizer.encode(text, truncation=True, return_tensors='pt')
+        # Tokenize the input text
+        input_ids = tokenizer.encode(text, truncation=True, return_tensors='pt')
 
-    # Generate the summary
-    summary_ids = model.generate(input_ids, num_beams=4, max_length=100, early_stopping=True)
-    summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+        # Generate the summary
+        summary_ids = model.generate(input_ids, num_beams=4, max_length=100, early_stopping=True)
+        summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
 
-    return summary
+        return summary
+    except Exception as e:
+        st.error("An error occurred during text summarization. Please try again later.")
+        st.error(str(e))
 
 def main():
     st.title("Text Summarizer")
@@ -25,9 +29,9 @@ def main():
     # Summarize the text when the user clicks the button
     if st.button("Summarize"):
         summary = summarize_text(text)
-        st.subheader("Summary:")
-        st.write(summary)
+        if summary:
+            st.subheader("Summary:")
+            st.write(summary)
 
 if __name__ == "__main__":
     main()
-
